c8ff5e7f5085a57bf685e290e0f1fcc47580cba6
Chandra shekar <csr6293@gmail.com>
2023-02-22 14:29:53 -0500

    CSS: item wrap for products container

--- src/components/dashboard/sidebar/index.js
+++ src/components/dashboard/sidebar/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Filter from './Filter';
+import Search from './Search';
+
 const SideBar = () => {
 
+  return (
+    <div>
+      <Filter />
+      <Search />
+    </div>
+  )
 }
 
 export default SideBar;
