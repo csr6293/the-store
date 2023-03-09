3f502dd6ca50614937eb0ddb7165f5c9719652be
Chandra shekar <csr6293@gmail.com>
2023-03-08 23:33:47 -0500

    Navbar in product details

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Navbar from './components/navbar';
 import Dashboard from './components/dashboard';
 import SideBar from './components/dashboard/sidebar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <div className='main'>
         <SideBar />
         <Dashboard />
--- src/RouteX.js
+++ src/RouteX.js
@@ -1,19 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import App from './App';
 import ProductDetail from "./components/dashboard/ProductDetail";
+import NavBar from "./components/navbar";
 
 const RouteX = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<RouteWithNavBar/>}>
         <Route exact path="products/:id" element={<ProductDetail />} />
         <Route exact path="products" element={<ProductDetail />} />
-        <Route exact path="/" element={<App />} />
         <Route exact index element={<App />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 };
+
+function RouteWithNavBar() {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  )
+}
 
 export default RouteX;
