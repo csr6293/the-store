a20e60b7382ea5143f6f006d6b1d600c7ee77068
Chandra shekar <csr6293@gmail.com>
2023-03-13 19:38:50 -0400

    Add link to contactus page in routes

--- src/RouteX.js
+++ src/RouteX.js
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import App from './App';
-import ProductDetail from "./components/dashboard/ProductDetail";
 import NavBar from "./components/navbar";
+import ContactUs from "./components/contact-us";
+import ProductDetail from "./components/dashboard/ProductDetail";
 
 const RouteX = () => {
   return (
@@ -10,6 +11,7 @@ const RouteX = () => {
       <Routes>
         <Route path="/" element={<RouteWithNavBar/>}>
         <Route exact path="products/:id" element={<ProductDetail />} />
+        <Route exact path="contact-us" element={<ContactUs />} />
         <Route exact path="products" element={<ProductDetail />} />
         <Route exact index element={<App />} />
         </Route>
