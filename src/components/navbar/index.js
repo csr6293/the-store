95ef96a313ba45b4f16d6bd0c17a7ba806afc3f4
Chandra shekar <csr6293@gmail.com>
2023-03-14 09:44:15 -0400

    Add coming soon page

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,14 +6,14 @@ import './index.css';
 const NavBar = () => {
   const navigate = useNavigate();
 
-  const navigateTo = (url) => {
+  const navigateTo = (url = '/') => {
     return navigate(url);
-  }
+  };
 
   return (
     <div className='navbarContainer'>
       <div className='name'>
-        <div onClick={() => navigateTo('/')}>The Store</div>
+        <div id='name' onClick={() => navigateTo('/')}>The Store</div>
         <div className='user-options'>
           <div>Login</div>
           <div>Wishlist</div>
@@ -22,10 +22,14 @@ const NavBar = () => {
       </div>
       <div className='navbar'>
         <ul className='nav-menu'>
-          <li>Products</li>
+          <li onClick={() => navigateTo()}>Products</li>
           <li onClick={() => navigateTo('/locations')}>Store Locator</li>
-          <li>Offers</li>
-          <li>Sale</li>
+          <li style={{
+            textDecoration: 'line-through'
+          }} onClick={() => navigateTo('/offers')}>Offers</li>
+          <li style={{
+            textDecoration: 'line-through'
+          }} onClick={() => navigateTo('/sales')}>Sale</li>
           <li>About us</li>
           <li onClick={() => navigateTo('/contact-us')}>Contact us</li>
         </ul>
---
+++ src/components/offers/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './index.css'
+
+const Offers = () => {
+
+  return (
+    <div className='cs-wrapper'>
+      <div>COMING SOON</div>
+    </div>
+  )
+}
+
+export default Offers;
