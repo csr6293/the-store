d19377c6eb337323a5a39c1af6bca4d470910566
Chandra shekar <csr6293@gmail.com>
2023-03-14 09:44:34 -0400

    Add sale and offers routes

--- src/RouteX.js
+++ src/RouteX.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 import App from './App';
 import NavBar from "./components/navbar";
+import Offers from "./components/offers";
 import ContactUs from "./components/contact-us";
 import StoreLocator from "./components/store-locator";
 import ProductDetail from "./components/dashboard/ProductDetail";
@@ -15,6 +16,8 @@ const RouteX = () => {
         <Route exact path="contact-us" element={<ContactUs />} />
         <Route exact path="locations" element={<StoreLocator />} />
         <Route exact path="products" element={<ProductDetail />} />
+        <Route exact path="offers" element={<Offers />} />
+        <Route exact path="sales" element={<Offers />} />
         <Route exact index element={<App />} />
         </Route>
       </Routes>
