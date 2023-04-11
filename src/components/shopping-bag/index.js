3e4d305c3c927f894301c45af0c13c632f00a276
Chandra shekar <csr6293@gmail.com>
2023-04-11 00:47:13 -0400

    Add initial comp for shopping cart

---
+++ src/components/shopping-bag/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ShoppingBag = () => {
+
+  return (
+    <div>
+      <div>
+        <h2>Your Shopping Cart</h2>
+        <div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ShoppingBag;
