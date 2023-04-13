0849cabac7cdc99b0ff3545f90d99f259acaa17e
Chandra shekar <csr6293@gmail.com>
2023-04-12 21:56:04 -0400

    List cart items in shopping cart

--- src/components/shopping-bag/index.js
+++ src/components/shopping-bag/index.js
@@ -2,16 +2,47 @@ import React from "react";
 
 const ShoppingBag = () => {
 
+  const [cartItems, setCartItems] = React.useState([{}]);
+
+  const getCartItemFromLS = () => {
+    const items = localStorage.getItem('products');
+
+    console.log('--', cartItems);
+
+    if (items) {
+      setCartItems(JSON.parse(items));
+    }
+  };
+
+  React.useEffect(() => {
+    getCartItemFromLS();
+  }, []);
+
   return (
     <div>
       <div>
         <h2>Your Shopping Cart</h2>
         <div>
+          {cartItems.map((item) => {
+            return <div>
+              <img src={item.images?.[0]} alt="pic" height={'100px'} width={'100px'} />
+              <div style={{
+                display: 'flex'
+              }}>
+                <h5>{item.title}</h5>
+                <h6>{item.description}</h6>
+                <h5>{item.price}</h5>
+              </div>
+              <div>
+                remove
+              </div>
+            </div>
+          })}
 
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ShoppingBag;
